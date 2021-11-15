41342e704473aa056d2de3d5009044ae767dff12
Steve Brown <steve.brown+github@brownserve.co.uk>
2021-11-15 19:05:23 +0000

    Add some module tests

---
+++ .build/tests/Module.Tests.ps1
@@ -0,0 +1,21 @@
+#requires -Modules Pester
+#.SYNOPSIS
+#   Performs tests to make sure the PowerShell module works as intended
+BeforeAll {
+    # Remove the module we've already imported 😬
+    Remove-Module PuppetPowerShell -Verbose:$false
+}
+Describe 'ModuleImport' {
+    Context 'When PuppetPowerShell is imported' {
+        It 'should not throw any exception' {
+            { Join-Path $global:BuiltModuleDirectory -ChildPath "PuppetPowerShell.psd1" | Import-Module -Force -Verbose:$false } | Should -not -Throw 
+        }
+        It 'should have cmdlets on the path' -TestCases @(
+            @{Filter = 'Install-Puppet'; Expected = 'Install-Puppet' }
+        ) {
+            param ($Filter, $Expected)
+            $Commands = Get-Command -name $Filter
+            $Commands.Name | Should -Be $Expected
+        }
+    }
+}
