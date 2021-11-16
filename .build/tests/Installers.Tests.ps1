87185c977515c00808af264f3b9f4bcb915a651e
Steve Brown <steve.brown+github@brownserve.co.uk>
2021-11-16 00:39:57 +0000

    I think the brackets got moved!

--- .build/tests/Installers.Tests.ps1
+++ .build/tests/Installers.Tests.ps1
@@ -13,72 +13,72 @@ Describe "Puppet installers" {
             $global:Containers += $tag
         }
     }
-}
-Context "Puppet agent installer" {
-    It "Should install on $global:OS" {
+    Context "Puppet agent installer" {
+        It "Should install on $global:OS" {
 
-        {
-            # on macOS we can't run the tests via Docker at the moment so use vagrant
-            if ($IsMacOS)
             {
-                Write-Verbose "macOS, using vagrant"
-                $Vagrantfile = Get-ChildItem (Join-Path $global:RepoTestsDirectory 'Vagrant' 'Vagrantfile')
-                Push-Location
-                Set-Location $Vagrantfile.PSParentPath
-                & vagrant up puppetagent-macos
-                if ($LASTEXITCODE -ne 0)
+                # on macOS we can't run the tests via Docker at the moment so use vagrant
+                if ($IsMacOS)
                 {
-                    Pop-Location
-                    throw "Failed to bring up Vagrant environment"
-                }
-                & vagrant ssh puppetagent-macos -c "pwsh -c 'Import-Module /usr/local/vagrant/Module/PuppetPowerShell.psm1 -Force; Install-Puppet -MajorVersion 6; Install-Puppet -Application puppet-bolt'"
-                if ($LASTEXITCODE -ne 0)
-                {
-                    $ErrorMessage = "Failed to install Puppet agent on macOS"
-                }
-                & vagrant destroy -f
-                Pop-Location
-                if ($ErrorMessage)
-                {
-                    throw $ErrorMessage
-                }
-            }
-            else
-            {
-
-                # Import the module and attempt to install Puppet agent!
-                # we should try to param the version of Puppet to install
-                $InstallScript = {
-                    if ($IsWindows)
-                    {
-                        $ModulePath = "C:\module\PuppetPowerShell.psm1"
-                    }
-                    else
-                    {
-                        $ModulePath = '/module/PuppetPowerShell.psm1'
-                    }
-                    Import-Module $ModulePath -Force
-                    if ($isLinux)
-                    {
-                        Install-Puppet -MajorVersion 6 -Application puppetserver
-                    }
-                    else
-                    {
-                        Install-Puppet -MajorVersion 6
-                    }
-                    Install-Puppet -Application puppet-bolt
-                }
-            
-                $global:Containers | ForEach-Object {
-                    # This volume mount will need tidying up for Windows containers...
-                    & docker run --rm -v $global:RepoModuleDirectory/:/module $_ 'pwsh' '-Command' "$InstallScript"
-                    # Do another test for exact version?
+                    Write-Verbose "macOS, using vagrant"
+                    $Vagrantfile = Get-ChildItem (Join-Path $global:RepoTestsDirectory 'Vagrant' 'Vagrantfile')
+                    Push-Location
+                    Set-Location $Vagrantfile.PSParentPath
+                    & vagrant up puppetagent-macos
                     if ($LASTEXITCODE -ne 0)
                     {
-                        throw "Failed to install Puppet agent on $_"
+                        Pop-Location
+                        throw "Failed to bring up Vagrant environment"
+                    }
+                    & vagrant ssh puppetagent-macos -c "pwsh -c 'Import-Module /usr/local/vagrant/Module/PuppetPowerShell.psm1 -Force; Install-Puppet -MajorVersion 6; Install-Puppet -Application puppet-bolt'"
+                    if ($LASTEXITCODE -ne 0)
+                    {
+                        $ErrorMessage = "Failed to install Puppet agent on macOS"
+                    }
+                    & vagrant destroy -f
+                    Pop-Location
+                    if ($ErrorMessage)
+                    {
+                        throw $ErrorMessage
                     }
                 }
-            }
-        } | Should -not -Throw 
+                else
+                {
+
+                    # Import the module and attempt to install Puppet agent!
+                    # we should try to param the version of Puppet to install
+                    $InstallScript = {
+                        if ($IsWindows)
+                        {
+                            $ModulePath = "C:\module\PuppetPowerShell.psm1"
+                        }
+                        else
+                        {
+                            $ModulePath = '/module/PuppetPowerShell.psm1'
+                        }
+                        Import-Module $ModulePath -Force
+                        if ($isLinux)
+                        {
+                            Install-Puppet -MajorVersion 6 -Application puppetserver
+                        }
+                        else
+                        {
+                            Install-Puppet -MajorVersion 6
+                        }
+                        Install-Puppet -Application puppet-bolt
+                    }
+            
+                    $global:Containers | ForEach-Object {
+                        # This volume mount will need tidying up for Windows containers...
+                        & docker run --rm -v $global:RepoModuleDirectory/:/module $_ 'pwsh' '-Command' "$InstallScript"
+                        # Do another test for exact version?
+                        if ($LASTEXITCODE -ne 0)
+                        {
+                            throw "Failed to install Puppet agent on $_"
+                        }
+                    }
+                }
+            } | Should -not -Throw 
+        }
     }
 }
